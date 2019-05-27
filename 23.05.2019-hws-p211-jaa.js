06f82f64a0dc9cbacce9f01da5da1e317ab18926
Jeyhun-Alizada <49520836+Jeyhun-Alizada@users.noreply.github.com>
2019-05-27 13:25:26 +0400

    Add files via upload

---
+++ 23.05.2019-hws-p211-jaa.js
@@ -0,0 +1,52 @@
+// HomeWork#1 End
+
+// HomeWork#2 Start
+
+// const forMapArray1 = [1, 4, 9, 16];
+// const map = forMapArray1.map(i => i * 2);
+// console.log(map);
+
+// HomeWork#2 End
+
+// HomeWork#3 Start
+
+// const forMapArray2 = ["Tebriz", "Baki", "Ankara"];
+// const map = forMapArray2.map(i => i.slice(i[0], i.length/2));
+// console.log(map);
+
+// HomeWork#3 End
+
+// HomeWork#4 Start
+
+// const forReduceArray1 = [1, 2, 3, 4, 8, 10];
+
+// forReduceArray1.filter(isEven);
+
+// function isEven(number) {
+//   return number % 2 === 0;
+// }
+
+// const forReduceArrayPro = forReduceArray1.filter(isEven);
+
+// const reducer = (allAdd, isEven) => allAdd + isEven;
+
+// console.log(forReduceArrayPro.reduce(reducer));
+
+// HomeWork#4 End
+
+// Home Work #5 Start
+
+// const forReduceArray2 = [7, 10, 12, 20, 26, 30];
+
+// function disiblabyfive(number) {
+//   return number % 5 === 0;
+// }
+
+// const forReduceArrayPro2 = forReduceArray2.filter(disiblabyfive);
+
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// console.log(forReduceArrayPro2.reduce(reducer));
+
+
+// Home Work #5 End
